eac83741f9e37cdb6a42987cbe774c7e75fe5385
Fauzan <fauzanfebriann@gmail.com>
2026-04-01 22:36:48 +0700

    feat(fill): Implement Fill.asm to blacken screen on key press

--- projects/4/fill/Fill.asm
+++ projects/4/fill/Fill.asm
@@ -3,9 +3,52 @@
 // by Nisan and Schocken, MIT Press.
 // File name: projects/4/Fill.asm
 
-// Runs an infinite loop that listens to the keyboard input. 
+// Runs an infinite loop that listens to the keyboard input.
 // When a key is pressed (any key), the program blackens the screen,
-// i.e. writes "black" in every pixel. When no key is pressed, 
+// i.e. writes "black" in every pixel. When no key is pressed,
 // the screen should be cleared.
 
-//// Replace this comment with your code.
+
+(LOOP)
+    @SCREEN
+    D=A
+    @address
+    M=D
+    @KBD
+    D=M
+    @BLACK
+    D;JGT
+    @WHITE
+    0;JMP
+(BLACK)
+    @address
+    A=M
+    M=-1
+    @1
+    D=A
+    @address
+    M=M+D
+    @KBD
+    D=A
+    @address
+    D=D-M
+    @BLACK
+    D;JGT
+    @LOOP
+    0;JMP
+(WHITE)
+    @address
+    A=M
+    M=0
+    @1
+    D=A
+    @address
+    M=M+D
+    @KBD
+    D=A
+    @address
+    D=D-M
+    @WHITE
+    D;JGT
+    @LOOP
+    0;JMP
